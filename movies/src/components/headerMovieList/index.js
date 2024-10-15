fc76fe0e86560dc1302d650d871faacc21c8c483
MichaelKehoe4318 <Michaelkehoecolab@gmail.com>
2024-10-15 12:19:48 +0100

    fix back/forward arrows

--- movies/src/components/headerMovieList/index.js
+++ movies/src/components/headerMovieList/index.js
@@ -4,9 +4,11 @@ import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
 
 const Header = (props ) => {
   const title = props.title
+  const navigate = useNavigate();
   return (
     <Paper 
       component="div" 
@@ -26,9 +28,7 @@ const Header = (props ) => {
       <Typography variant="h4" component="h3">
         {title}
       </Typography>
-      <IconButton
-        aria-label="go forward"
-      >
+      <IconButton aria-label="go forward" onClick={() => navigate(+1)}>
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
     </Paper>
